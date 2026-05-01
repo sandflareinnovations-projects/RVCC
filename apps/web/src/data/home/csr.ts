e45a6a0b3db0d9cd6401285d29bacd8ff0cf080e
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-01 11:01:43 +0530

    feat: update certificate data with new high-res assets and correct mapping

--- apps/web/src/data/home/csr.ts
+++ apps/web/src/data/home/csr.ts
@@ -3,10 +3,26 @@ import { Certificate, SisterCompany } from "@types";
 export type { Certificate, SisterCompany };
 
 export const certificates: Certificate[] = [
-  { name: "ISO 9001", code: "ISO-9001-2015" },
-  { name: "ISO 14001", code: "ISO-14001-2015" },
-  { name: "OHSAS 18001", code: "OHSAS-18001-2007" },
-  { name: "ISO 45001", code: "ISO-45001-2018" },
+  {
+    name: "ISO 9001",
+    code: "ISO - 9001 - 2008",
+    image: "/certificate/tuv.png",
+  },
+  {
+    name: "ISO 14001",
+    code: "ISO - 14001 - 2004",
+    image: "/certificate/jas-anz-14001.png",
+  },
+  {
+    name: "OHSAS 18001",
+    code: "OHSAS - 18001 - 2007",
+    image: "/certificate/jas-anz-18001.png",
+  },
+  {
+    name: "IAF",
+    code: "Member of Multilateral",
+    image: "/certificate/iaf.png",
+  },
 ];
 
 export const sisterCompanies: SisterCompany[] = [
