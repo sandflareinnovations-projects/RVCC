e027b276018f8b2241a0a6d6b5d6663d11d748e3
ajmal faris <ajmalfaris.me@gmail.com>
2026-09-07 17:48:26 +0530

    refactor(api): configure @ path alias and export domain module barrels

--- apps/api/src/modules/content/services/index.ts
+++ apps/api/src/modules/content/services/index.ts
@@ -1,12 +1,13 @@
 /**
  * Website CMS Content Business Logic Services
  */
-export * from "../../public/careers";
-export * from "../../public/clients";
-export * from "../../public/companies";
-export * from "../../public/documents";
-export * from "../../public/gallery";
-export * from "../../public/hero";
-export * from "../../public/media";
-export * from "../../public/projects";
-export * from "../../public/services";
+export * from "@/modules/public/careers";
+export * from "@/modules/public/clients";
+export * from "@/modules/public/companies";
+export * from "@/modules/public/documents";
+export * from "@/modules/public/gallery";
+export * from "@/modules/public/hero";
+export * from "@/modules/public/media";
+export * from "@/modules/public/projects";
+export * from "@/modules/public/services";
+
---
+++ apps/api/src/modules/index.ts
@@ -0,0 +1,6 @@
+export * as AuthModule from "./auth";
+export * as BiddingModule from "./bidding";
+export * as ContentModule from "./content";
+export * as EnquireModule from "./enquire";
+export * as ProcurementModule from "./procurement";
+export * as VendorModule from "./vendor";
